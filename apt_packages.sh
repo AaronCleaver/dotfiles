8baf5c5597d7e9b45184ecdccffb31aa0abad331
Aaron Cleaver <aaron@cleaver.net.au>
2019-11-11 20:07:52 +1100

    Update apt_packages.sh

--- apt_packages.sh
+++ apt_packages.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-sed -i -e "s@http://archive.ubuntu.com/ubuntu/@http://mirror.aarnet.edu.au/pub/ubuntu/archive/" /etc/apt/sources.list
+sed -i -e "s@http://archive.ubuntu.com/ubuntu/@http://mirror.aarnet.edu.au/pub/ubuntu/archive/@" /etc/apt/sources.list
 
 apt update
 apt upgrade -y
@@ -11,11 +11,10 @@ apt install -y build-essential clang bison flex libreadline-dev gawk tcl-dev lib
 apt install -y -qq g++ gcc libc6-dev libffi-dev libgmp-dev make xz-utils zlib1g-dev git gnupg libtinfo-dev
 apt install -y global tree bc iverilog gtkwave colordiff dos2unix cmake clang cgdb vim ctags autoconf gperf
 apt install -y build-essential fakeroot autotools-dev bison bsdmainutils debhelper dh-strip-nondeterminism file flex gettext gettext-base gperf groff-base intltool-debian libarchive-zip-perl libasprintf0v5 libbison-dev libbz2-dev libcroco3 libfile-stripnondeterminism-perl libfl-dev libglib2.0-0 libmagic1 libpipeline1 libreadline-dev libreadline6-dev libsigsegv2 libtimedate-perl libtinfo-dev libxml2 m4 man-db po-debconf zlib1g-dev
-apt install -y cscope zip
+apt install -y cscope zip mosh python3 python3-pip python3-setuptools
+apt install -y xpra xserver-xephyr xinit xauth xclip x11-xserver-utils x11-utils nxagent
 apt install -y docker.io
 service docker start
-apt install -y python3 python3-pip python3-setuptools
 pip3 install wheel
 pip3 install pynvim
-apt install -y xpra xserver-xephyr xinit xauth xclip x11-xserver-utils x11-utils nxagent
 curl -fsSL https://raw.githubusercontent.com/mviereck/x11docker/master/x11docker | bash -s -- --update
